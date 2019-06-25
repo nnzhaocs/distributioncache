736886d08b4477118b6c3b2b46ef4bdf7e9e1a52
nnzhaocs <znannan1@vt.edu>
2019-06-25 16:10:54 -0400

    update

--- registry/storage/blobserver.go
+++ registry/storage/blobserver.go
@@ -579,11 +579,8 @@ func (bs *blobServer) ServeBlob(ctx context.Context, w http.ResponseWriter, r *h
 		context.GetLogger(ctx).Errorf("NANNAN: %s ", err)
 	}
 	context.GetLogger(ctx).Debugf("NANNAN: slice cache put: %v B for %s", len(bfss), dgst.String())
-<<<<<<< HEAD
+
 	if len(bfss) > 0 {	
-=======
-	if len(bfss) > 0 {
->>>>>>> d26bf024b936cf981f4ff8ff34ef0b07cc729c9b
 		err = bs.cache.Dc.Put(dgst.String(), bfss)
 		if err != nil {
 			context.GetLogger(ctx).Debugf("NANNAN: slice cache cannot write to digest: %v: %v ", dgst.String(), err)
