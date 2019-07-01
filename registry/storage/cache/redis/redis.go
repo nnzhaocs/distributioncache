4156ccb9c7e2de1036da8e1ebe4f4463c1d2e6cf
nnzhaocs <znannan1@vt.edu>
2019-07-01 17:51:17 -0400

    update

--- registry/storage/cache/redis/redis.go
+++ registry/storage/cache/redis/redis.go
@@ -383,7 +383,7 @@ func (rfds *redisFileDescriptorService) BFRecipeHashKey(dgst digest.Digest) stri
 }
 
 func (rfds *redisFileDescriptorService) BSRecipeHashKey(dgst digest.Digest, server string) string {
-	return "Blob:File:Recipe::" + dgst.String() + "::" + server
+	return "Blob:Slice:Recipe::" + dgst.String() + "::" + server
 }
 
 func (rfds *redisFileDescriptorService) StatBFRecipe(ctx context.Context, dgst digest.Digest) (distribution.BFRecipeDescriptor, error) {
