9edec47fe3e82866577ce405ff4329b0afdebb4c
nnzhaocs <znannan1@vt.edu>
2019-07-25 13:55:38 -0400

    update

--- registry/storage/driver/gcache/blobcache.go
+++ registry/storage/driver/gcache/blobcache.go
@@ -107,7 +107,7 @@ func (cache *BlobCache) Init() error {
 	cache.SliceLST = SliceLST
 	
 	// stage area *****
-	StageLSTCAP := 10*1024 //10gb
+	StageLSTCAP := 1 //10gb
 	StageLST := New(StageLSTCAP * 1024 * 1024).LRU().
 		Expiration(3600 * time.Minute).
 		Build()
